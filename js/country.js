a87e964c1f64e5996d9096ceb268f6102caa3846
antar-cd <sagar.chandradas1994@gmail.com>
2023-03-22 19:07:41 +0600

    add 3type of destructuring

--- js/country.js
+++ js/country.js
@@ -10,11 +10,33 @@ const displayCountries = countries =>{
     const container =document.getElementById('countries');
     container.innerHTML =countriesHTML.join(' ')
 }
-const getcountryHtml = country =>{
+/* //orginal
+ const getcountryHtml = country =>{
     return `
     <div class='country'>
         <h2>${country.name.common}</h2>
         <img src="${country.flags.png}">
+    </div>
+    `
+} */
+//option-1
+/* const getcountryHtml = country =>{
+    const {name,flags} =country
+    return `
+    <div class='country'>
+        <h2>${name.common}</h2>
+        <img src="${flags.png}">
+    </div>
+    `
+} */
+// option 2
+const getcountryHtml = ({name,flags,area,region}) =>{
+    return `
+    <div class='country'>
+        <h2>${name.common}</h2>
+        <p>Area:${area}</p>
+        <p>continet:${region}</p>
+        <img src="${flags.png}">
     </div>
     `
 }
